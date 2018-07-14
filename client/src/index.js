0c0e348d9e7c0ca689687e32747892c156d97af4
John Huynh <John.huynh@student.qut.edu.au>
2018-07-14 23:13:52 +1000

    Added: The required files (Cannot run yet)

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import promise from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import registerServiceWorker from "./registerServiceWorker";
+import reducers from "./reducers";
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          {/* <Route path="/results/:post" component={PostNew} />
+          <Route path="/location/:id" component={PostShow} />
+          <Route path="/login/" component={Login} />
+          <Route path="/register" component={register} /> */}
+          <Route path="/" component={PostIndex} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
+
 registerServiceWorker();
