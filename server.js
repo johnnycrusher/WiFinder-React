8f554273e2258ed13b07572f8add3c8be3bd384c
John Huynh <John.huynh@student.qut.edu.au>
2018-07-14 21:35:07 +1000

    Added: User Registration Post Method

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-
 const mysql = require("mysql");
+const bcrypt = require("bcryptjs");
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -17,6 +17,9 @@ db.connect(err => {
 });
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen("3000", () => console.log("Server started on port 3000"));
 
@@ -58,6 +61,27 @@ app.get("/api/AdvanceSearch/", (req, res) => {
   );
 });
 
+app.get("/api/BasicSearch/", (req, res) => {
+  postData = req.query;
+  let sql = `SELECT w.LocationID, w.LocationName, w.LocationType, w.LocationAddress, w.LocationSuburb, w.Latitude, w.Longitude,
+      (3959 * acos(cos(radians(?))*cos(radians(Latitude))*cos(radians(Longitude)-radians(?))+sin(radians(?))*sin(radians(Latitude))))
+      AS distance, ifnull(round(avg(r.ReviewRating),1),0) AS AvgRating FROM wifi_location w
+      LEFT JOIN reviews r
+      ON w.LocationID = r.LocationID
+      GROUP BY LocationID
+      HAVING distance<10000
+      ORDER BY distance LIMIT 0,20`;
+
+  let query = db.query(
+    sql,
+    [postData.Latitude, postData.Longitude, postData.Latitude],
+    (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    }
+  );
+});
+
 app.get("/api/location/:id", (req, res) => {
   let sql = `SELECT w.LocationID, w.LocationName, w.LocationType, w.LocationAddress, w.LocationSuburb, w.Latitude, w.Longitude, 
   ifnull(round(avg(r.ReviewRating),1),0) AS AvgRating
@@ -69,5 +93,63 @@ app.get("/api/location/:id", (req, res) => {
   let query = db.query(sql, [req.params.id], (err, result) => {
     if (err) throw err;
     res.send(result);
+  });
+});
+
+app.post("/api/user/register", (req, res) => {
+  let bodyData = req.body;
+  console.log(bodyData);
+  let FirstName = req.body.FirstName;
+  let LastName = req.body.LastName;
+  let UserName = req.body.UserName;
+  let EmailAddress = req.body.EmailAddress;
+  var Password = req.body.Password;
+  var Salt;
+  bcrypt.genSalt(10, (err, salt) => {
+    var PasswordSalt = salt;
+    bcrypt.hash(Password, salt, (err, hash) => {
+      if (err) throw err;
+      var PasswordHash = hash;
+      console.log(`Password:${PasswordHash}  Salt:${PasswordSalt}`);
+      let sql = `INSERT INTO users(FirstName,LastName,UserName,EmailAddress,PasswordSalt,PasswordHash)
+      VALUES(?,?,?,?,?,?)`;
+      let getID = `Select UserID From Users WHERE UserName = ?`;
+      let userSql = `INSERT INTO membership(UserID,UserName,Membership)
+      values(?,?,?)`;
+      var userID = 0;
+      console.log(`FirstName:${FirstName}`);
+      console.log(`LastName:${LastName}`);
+      console.log(`UserName:${UserName}`);
+      console.log(`EmailAddress:${EmailAddress}`);
+      console.log(`Password:${PasswordHash}`);
+      console.log(`Salt:${PasswordSalt}`);
+      let query = db.query(
+        sql,
+        [
+          FirstName,
+          LastName,
+          UserName,
+          EmailAddress,
+          PasswordSalt,
+          PasswordHash
+        ],
+        (err, result) => {
+          if (err) throw err;
+        }
+      );
+      let queryTwo = db.query(getID, [UserName], (err, result) => {
+        if (err) throw err;
+        userID = result[0].UserID;
+        let queryThree = db.query(
+          userSql,
+          [userID, UserName, "Standard"],
+          (err, result) => {
+            console.log(userID);
+            if (err) throw err;
+            res.send(result);
+          }
+        );
+      });
+    });
   });
 });
